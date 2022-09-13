f5fa678663d507851e624b1607c8a827569b54a5
yeony402 <44489399+yeony402@users.noreply.github.com>
2022-09-13 15:28:16 +0900

    Update MemberService.java

--- src/main/java/com/example/intermediate/service/MemberService.java
+++ src/main/java/com/example/intermediate/service/MemberService.java
@@ -66,7 +66,7 @@ public class MemberService {
     }
 
     public ResponseDto<?> logout(HttpServletRequest request) {
-        if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
+        if (!tokenProvider.validateToken(request.getHeader("RefreshToken"))) {
             return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
         }
         Member member = tokenProvider.getMemberFromAuthentication();
@@ -86,7 +86,7 @@ public class MemberService {
 
     public void tokenToHeaders(TokenDto tokenDto, HttpServletResponse response) {
         response.addHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
-        response.addHeader("Refresh-Token", tokenDto.getRefreshToken());
+        response.addHeader("RefreshToken", tokenDto.getRefreshToken());
         response.addHeader("Access-Token-Expire-Time", tokenDto.getAccessTokenExpiresIn().toString());
     }
 
